164d73f76ced94d4fc57388f7e04619d225a1d3d
MichaelKhadzhynov <khadzhynovm@gmail.com>
2023-01-12 19:28:45 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private final static Logger LOGGER = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("new project");
+        LOGGER.error("new project");
+    }
+}
